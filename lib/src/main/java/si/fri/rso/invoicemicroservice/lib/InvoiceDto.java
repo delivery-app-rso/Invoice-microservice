f31b61e9535fec75e4801511f745788a74640829
Gal Lindič <2stskier@gmail.com>
2022-11-18 09:44:40 +0100

    Added InvoiceDto

---
+++ lib/src/main/java/si/fri/rso/invoicemicroservice/lib/InvoiceDto.java
@@ -0,0 +1,14 @@
+package si.fri.rso.invoicemicroservice.lib;
+
+public class InvoiceDto {
+    //TODO: Add additional required data
+    private Integer userId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
